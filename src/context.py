e605d9780e34fb01d12945797ac3fa15b1d8fe3c
morganllewellynjones <mjones23@uoregon.edu>
2024-02-05 19:20:15 -0800

    Added description to context file.

--- src/context.py
+++ src/context.py
@@ -1,3 +1,10 @@
+'"""
+Authors: Morgan Jones and Alex Santos
+Last Updated: 02/02/04
+
+This file can be imported to change the working directory of executing code to the src directory. This stabilizes the paths of directories which import or output data files (such as processData.py).
+"""
+
 import os
 src_dir = os.path.abspath(os.path.dirname(__file__))
 os.chdir(src_dir)
